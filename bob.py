542cbbc4edec77cc7dd6bddd6585b9997da57d9b
luiisx <luisbelit@gmail.com>
2025-01-09 17:13:14 -0500

    changed text

--- bob.py
+++ bob.py
@@ -11,7 +11,7 @@ class Client(discord.Client):
     async def on_ready(self):
         print(f'{self.user} has connected to Discord yay!')
     async def on_message(self,message):
-        print(f'Message from {message.author}: {message.content}')
+        print(f'Message by {message.author}: {message.content}')
 
 intents = discord.Intents.default()
 intents.message_content = True
