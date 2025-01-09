7deef65f34bfb79804d53e46693ea9206a1c7d41
luiisx <luisbelit@gmail.com>
2025-01-09 17:04:44 -0500

    bob tracks discords messages

--- bob.py
+++ bob.py
@@ -10,7 +10,8 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 class Client(discord.Client):
     async def on_ready(self):
         print(f'{self.user} has connected to Discord yay!')
-
+    async def on_message(self,message):
+        print(f'Message from {message.author}: {message.content}')
 
 intents = discord.Intents.default()
 intents.message_content = True
