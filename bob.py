9dca0755602ad71be231c35766b9fd2a5baf1ad0
Karina Garces ✨ <karinadgarces@gmail.com>
2025-01-09 16:18:17 -0500

    bob is talking

--- bob.py
+++ bob.py
@@ -9,7 +9,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 class Client(discord.Client):
     async def on_ready(self):
-        print(f'{self.user} has connected to Discord!')
+        print(f'{self.user} has connected to Discord yay!')
 
 intents = discord.Intents.default()
 intents.message_content = True
