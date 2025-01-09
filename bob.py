48dc55aac333ea9a1f77f4de4df71d8ad4eae221
Karina Garces ✨ <karinadgarces@gmail.com>
2025-01-09 16:11:59 -0500

    bob is alive

---
+++ bob.py
@@ -0,0 +1,18 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run(TOKEN)
